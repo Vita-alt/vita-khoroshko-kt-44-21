785811ab1e486711046a0ba7c53480fd969a2f9c
khoroshko-vita-kt-44-21 <vita_khoroshko@mail.ru>
2024-11-21 16:17:17 +0300

    add logging and WeatherForecast test request

--- vita-khoroshko-kt-44-21/Program.cs
+++ vita-khoroshko-kt-44-21/Program.cs
@@ -1,8 +1,16 @@
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+try
+{
+	builder.Logging.ClearProviders();
+	builder.Host.UseNLog();
+	// Add services to the container.
 
-builder.Services.AddControllers();
+	builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,3 +29,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+}
+catch (Exception ex)
+{
+	logger.Error(ex, "Stopped program because of exception");
+}
+finally
+{
+	LogManager.Shutdown();
+}
